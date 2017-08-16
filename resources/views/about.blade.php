f994b41426ffd7a95d85f537023ff1cdc6fc713c
Max Nelson <maxanelson@gmail.com>
2017-08-16 00:43:42 -0700

    laracast blog initial setup

---
+++ resources/views/about.blade.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html>
+
+    <head>
+        <title>Dickbutt69</title>
+    </head>
+
+    <body>
+        <h1>about us</h1>
+    </body>
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html>
+
+    <head>
+        <title>Dickbutt69</title>
+    </head>
+
+    <body>
+        <h1>
+                    GIVE ME MONEY
+        </h1>
+
+    </body>
+</html>
